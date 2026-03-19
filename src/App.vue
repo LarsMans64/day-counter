e7d7b657e07c6d514580c26a2dbfcfda2d23c3e1
LarsMans <l.s.vanemmerik@gmail.com>
2026-03-19 12:43:04 +0100

    reload button

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,18 @@ const data = useLocalStorage<CounterData>("data", { days: [] });
 
 const days = data.value.days;
 
-while (!days.some(day => isToday(day.date))) {
-  const latest = days[0];
-  console.log(latest)
-  days.unshift({
-    date: latest ? getTomorrow(latest.date).valueOf() : new Date().valueOf(),
-    count: 0,
-  })
-}
-
 const most = computed(() => days.map(value => value.count).reduce((left, right) => Math.max(left, right)));
+
+function reload() {
+  while (!days.some(day => isToday(day.date))) {
+    const latest = days[0];
+    console.log(latest)
+    days.unshift({
+      date: latest ? getTomorrow(latest.date).valueOf() : new Date().valueOf(),
+      count: 0,
+    })
+  }
+}
 
 function getTomorrow(date: number) {
   const d = new Date(date);
@@ -35,9 +37,16 @@ function isToday(date: number) {
 
 <template>
   <div class="page">
-    <div class="days">
-      <Day v-for="day in data.days" :day="day" :most="most" class="day"/>
-    </div>
+    <main>
+      <div class="buttons">
+        <button @click="reload" class="reload">
+          Reload
+        </button>
+      </div>
+      <div class="days">
+        <Day v-for="day in data.days" :day="day" :most="most" class="day"/>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -48,13 +57,38 @@ function isToday(date: number) {
   padding: 10vh 10vw;
 }
 
+main {
+  max-width: 1000px;
+  margin-inline: auto;
+}
+
+.buttons {
+  display: flex;
+  gap: 10px;
+
+  margin-bottom: 50px;
+
+  button {
+    background-color: var(--col-button);
+    border: 5px solid var(--col-button-hover);
+    border-radius: 20px;
+    cursor: pointer;
+    padding: 15px 20px;
+    font-size: 1.1rem;
+    font-family: inherit;
+
+    transition: all 100ms ease-out;
+
+    &:hover {
+      background-color: var(--col-button-hover);
+    }
+  }
+}
+
 .days {
   display: flex;
   flex-wrap: wrap;
   gap: 10px;
-
-  max-width: 1000px;
-  margin-inline: auto;
 }
 
 .day {
